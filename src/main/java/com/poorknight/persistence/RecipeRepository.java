29476d896099c834d9e4a46b887d5abeb9c6c80b
Chris Atkins <chrisatkins55@gmail.com>
2016-04-10 21:21:41 -0400

    adding findAll and delete to recipe repository

--- src/main/java/com/poorknight/persistence/RecipeRepository.java
+++ src/main/java/com/poorknight/persistence/RecipeRepository.java
@@ -1,11 +1,15 @@
 package com.poorknight.persistence;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.poorknight.domain.Recipe;
 import com.poorknight.domain.identities.RecipeId;
@@ -21,22 +25,38 @@ public class RecipeRepository {
 	}
 
 	public Recipe saveNewRecipe(final Recipe recipe) {
-		final MongoCollection<Document> collection = buildCollection();
+		final MongoCollection<Document> collection = getRecipeCollection();
 		final Document document = toDocument(recipe);
 		collection.insertOne(document);
 		return toRecipe(document);
 	}
 
 	public Recipe findRecipeById(final RecipeId id) {
-		final MongoCollection<Document> collection = buildCollection();
-		final Bson filter = com.mongodb.client.model.Filters.eq("_id", new ObjectId(id.getValue()));
-		return toRecipe(collection.find(filter).first());
+		final MongoCollection<Document> collection = getRecipeCollection();
+		final Bson idFilter = createFilterOnId(id);
+		final Document document = collection.find(idFilter).first();
+		return toRecipe(document);
 	}
 
-	private MongoCollection<Document> buildCollection() {
+	public List<Recipe> findAllRecipes() {
+		final MongoCollection<Document> collection = getRecipeCollection();
+		final MongoCursor<Document> recipeIterator = collection.find().iterator();
+		return toRecipeList(recipeIterator);
+	}
+
+	public void deleteRecipe(final RecipeId id) {
+		final MongoCollection<Document> collection = getRecipeCollection();
+		final Bson idFilter = createFilterOnId(id);
+		collection.deleteOne(idFilter);
+	}
+
+	private MongoCollection<Document> getRecipeCollection() {
 		final MongoDatabase database = this.mongoClient.getDatabase(DB_NAME);
-		final MongoCollection<Document> collection = database.getCollection(RECIPE_COLLECTION);
-		return collection;
+		return database.getCollection(RECIPE_COLLECTION);
+	}
+
+	private Bson createFilterOnId(final RecipeId id) {
+		return com.mongodb.client.model.Filters.eq("_id", new ObjectId(id.getValue()));
 	}
 
 	private Document toDocument(final Recipe recipe) {
@@ -50,5 +70,14 @@ public class RecipeRepository {
 		final String content = document.getString("content");
 
 		return new Recipe(new RecipeId(id.toHexString()), name, content);
+	}
+
+	private List<Recipe> toRecipeList(final MongoCursor<Document> recipeIterator) {
+		final List<Recipe> recipeList = new LinkedList<>();
+		while (recipeIterator.hasNext()) {
+			final Document document = recipeIterator.next();
+			recipeList.add(toRecipe(document));
+		}
+		return recipeList;
 	}
 }
--- src/test/java/com/poorknight/persistence/RecipeRepositoryTest.java
+++ src/test/java/com/poorknight/persistence/RecipeRepositoryTest.java
@@ -3,6 +3,9 @@ package com.poorknight.persistence;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -49,5 +52,62 @@ public class RecipeRepositoryTest {
 		assertThat(foundRecipe.getId(), equalTo(savedRecipe.getId()));
 		assertThat(foundRecipe.getName(), equalTo("name"));
 		assertThat(foundRecipe.getContent(), equalTo("content"));
+	}
+
+	@Test
+	public void queryAllRecipesWorksAsExpected() throws Exception {
+		final Recipe recipe1 = new Recipe("queryAllRecipesWorksAsExpected_name1", "queryAllRecipesWorksAsExpected_content1");
+		final Recipe recipe2 = new Recipe("queryAllRecipesWorksAsExpected_name2", "queryAllRecipesWorksAsExpected_content2");
+		final Recipe recipe3 = new Recipe("queryAllRecipesWorksAsExpected_name3", "queryAllRecipesWorksAsExpected_content3");
+
+		recipeRepository.saveNewRecipe(recipe1);
+		recipeRepository.saveNewRecipe(recipe2);
+		recipeRepository.saveNewRecipe(recipe3);
+
+		final List<Recipe> recipeList = recipeRepository.findAllRecipes();
+
+		final Recipe foundRecipe1 = findRecipeByName("queryAllRecipesWorksAsExpected_name1", recipeList);
+		assertThat(foundRecipe1.getContent(), equalTo("queryAllRecipesWorksAsExpected_content1"));
+
+		final Recipe foundRecipe2 = findRecipeByName("queryAllRecipesWorksAsExpected_name2", recipeList);
+		assertThat(foundRecipe2.getContent(), equalTo("queryAllRecipesWorksAsExpected_content2"));
+
+		final Recipe foundRecipe3 = findRecipeByName("queryAllRecipesWorksAsExpected_name3", recipeList);
+		assertThat(foundRecipe3.getContent(), equalTo("queryAllRecipesWorksAsExpected_content3"));
+	}
+
+	private Recipe findRecipeByName(final String nameToFind, final List<Recipe> recipeList) {
+		for (final Recipe recipe : recipeList) {
+			if (recipe.getName().equals(nameToFind)) {
+				return recipe;
+			}
+		}
+		return null;
+	}
+
+	@Test
+	public void deleteWorks() throws Exception {
+		final Recipe recipe1 = new Recipe("deleteWorks_name1", "deleteWorks_content1");
+		final Recipe recipe2 = new Recipe("deleteWorks_name2", "deleteWorks_content2");
+		final Recipe recipe3 = new Recipe("deleteWorks_name3", "deleteWorks_content3");
+
+		final Recipe recipeToDelete = recipeRepository.saveNewRecipe(recipe1);
+		recipeRepository.saveNewRecipe(recipe2);
+		recipeRepository.saveNewRecipe(recipe3);
+
+		final List<Recipe> recipeList = recipeRepository.findAllRecipes();
+		final int initialSize = recipeList.size();
+		assertThat(findRecipeByName("deleteWorks_name1", recipeList), notNullValue());
+		assertThat(findRecipeByName("deleteWorks_name2", recipeList), notNullValue());
+		assertThat(findRecipeByName("deleteWorks_name3", recipeList), notNullValue());
+
+		recipeRepository.deleteRecipe(recipeToDelete.getId());
+
+		final List<Recipe> recipeListAfterDelete = recipeRepository.findAllRecipes();
+		assertThat(recipeListAfterDelete.size(), equalTo(initialSize - 1));
+
+		assertThat(findRecipeByName("deleteWorks_name1", recipeListAfterDelete), nullValue());
+		assertThat(findRecipeByName("deleteWorks_name2", recipeListAfterDelete), notNullValue());
+		assertThat(findRecipeByName("deleteWorks_name3", recipeListAfterDelete), notNullValue());
 	}
 }
