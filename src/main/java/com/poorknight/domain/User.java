552c0b74a41115ce303378d6f16a1ed8778271a8
Chris Atkins <chrisatkins55@gmail.com>
2016-05-28 18:12:02 -0400

    basic save and get of a user (repository level - no endpoint yet)

---
+++ src/main/java/com/poorknight/domain/User.java
@@ -0,0 +1,83 @@
+package com.poorknight.domain;
+
+import com.poorknight.domain.identities.UserId;
+
+public class User {
+
+	private final UserId id;
+	private final String name;
+	private final String email;
+
+	public User(final String name, final String email) {
+		this(null, name, email);
+	}
+
+	public User(final UserId id, final String name, final String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public UserId getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final User other = (User) obj;
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+}
---
+++ src/main/java/com/poorknight/persistence/UserRepository.java
@@ -0,0 +1,54 @@
+package com.poorknight.persistence;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.poorknight.app.init.MongoSetup;
+import com.poorknight.domain.User;
+import com.poorknight.domain.identities.UserId;
+
+public class UserRepository {
+
+	private final MongoClient mongoClient;
+
+	public UserRepository(final MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+
+	public User saveUser(final User userToSave) {
+		final MongoCollection<Document> userCollection = getUserCollection();
+
+		final Document document = toDocument(userToSave);
+		userCollection.insertOne(document);
+		return toUser(document);
+	}
+
+	public User findUserById(final UserId id) {
+		final MongoCollection<Document> userCollection = getUserCollection();
+		final Bson filter = Filters.eq("_id", new ObjectId(id.getValue()));
+		final Document result = userCollection.find(filter).first();
+		return toUser(result);
+	}
+
+	private Document toDocument(final User user) {
+		return new Document().append("name", user.getName()).append("email", user.getEmail());
+	}
+
+	private User toUser(final Document document) {
+		final UserId id = new UserId(document.getObjectId("_id").toHexString());
+		final String name = document.getString("name");
+		final String email = document.getString("email");
+
+		return new User(id, name, email);
+	}
+
+	private MongoCollection<Document> getUserCollection() {
+		final MongoDatabase database = this.mongoClient.getDatabase(MongoSetup.DB_NAME);
+		return database.getCollection(MongoSetup.USER_COLLECTION);
+	}
+}
