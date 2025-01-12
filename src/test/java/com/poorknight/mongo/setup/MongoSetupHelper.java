b8beec5dcbc2aa23cac706f7e0d6f3224331d56b
Chris <chrisatkins55@gmail.com>
2025-01-12 18:58:15 -0500

    Testcontainers instead of imbedded mongo, updated gradle

--- src/test/java/com/poorknight/mongo/setup/MongoSetupHelper.java
+++ src/test/java/com/poorknight/mongo/setup/MongoSetupHelper.java
@@ -8,48 +8,26 @@ import com.poorknight.image.ImageCollectionInitializer;
 import com.poorknight.recipe.RecipeCollectionInitializer;
 import com.poorknight.recipebook.RecipeBookCollectionInitializer;
 import com.poorknight.user.UserCollectionInitializer;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Feature;
-import de.flapdoodle.embed.mongo.distribution.Versions;
-import de.flapdoodle.embed.process.distribution.GenericVersion;
-import de.flapdoodle.embed.process.runtime.Network;
 import org.bson.Document;
+import org.testcontainers.containers.MongoDBContainer;
 
 public class MongoSetupHelper {
 
-	private static final String MONGO_VERSION = "3.2.4";
-
-	private static MongodExecutable mongodExecutable = null;
 	private static MongoClient mongo;
-	private static MongodProcess mongod;
+
+	static public MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
 
 	public static MongoClient startMongoInstance() throws Exception {
-		final MongodStarter starter = MongodStarter.getDefaultInstance();
-		final int port = Network.getFreeServerPort();
-		final IMongodConfig mongodConfig = new MongodConfigBuilder() //
-				.version(Versions.withFeatures(new GenericVersion(MONGO_VERSION), Feature.SYNC_DELAY)) //
-				.net(new Net(port, Network.localhostIsIPv6())) //
-				.build();
+		mongoDBContainer.start();
+		int port = mongoDBContainer.getFirstMappedPort();
 
-		mongodExecutable = starter.prepare(mongodConfig);
-		mongod = mongodExecutable.start();
 		mongo = new MongoClient("localhost", port);
 		MongoSetup.setupDatabaseCollections(mongo);
 		return mongo;
 	}
 
 	public static void cleanupMongo() {
-		try {
-			mongo.close();
-		} finally {
-			mongod.stop();
-			mongodExecutable.stop();
-		}
+		mongoDBContainer.stop();
 	}
 
 	public static void deleteAllRecipes() {
